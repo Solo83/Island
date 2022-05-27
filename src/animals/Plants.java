ab01a76b1f67a826ae13e40cff3cd5a60f415b6e
Solo83 <gstsamara@gmail.com>
2022-05-27 10:03:45 +0300

    Сorrection of mentor's remarks

---
+++ src/animals/Plants.java
@@ -0,0 +1,28 @@
+package animals;
+
+import animals.Population;
+
+public class Plants {
+
+    private static int allPlantsCount;
+
+    private String picture = Population.PLANTS.getPicture();
+
+    public Plants() {
+        allPlantsCount++;
+    }
+
+    public static int getAllPlantsCount() {
+        return allPlantsCount;
+    }
+
+    public static void decreasePlants() {
+        allPlantsCount--;
+    }
+
+    @Override
+    public String toString() {
+        //return  "[" + this.getClass().getSimpleName().toUpperCase() + "]";
+        return picture; // Если будут проблемы с кодировкой, раскомментировать верхний
+    }
+}
---
+++ src/controller/Turn.java
@@ -0,0 +1,40 @@
+package controller;
+
+import controller.Island;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Turn implements Runnable {
+
+    private static AtomicInteger id = new AtomicInteger(0);
+    private final Island island;
+
+    public Turn(Island island) {
+        this.island = island;
+
+    }
+
+    @Override
+    public void run() {
+
+        id.getAndIncrement();
+
+        Island.Cell[][] islandMap = Island.getIslandMap();
+
+        for (int i = 0; i < islandMap.length; i++) {
+            for (int j = 0; j < islandMap.length; j++) {
+                island.eatAllOnCell(islandMap[i][j]);
+                island.multiplyOnCell(islandMap[i][j]);
+                island.growPlants(islandMap[i][j]);
+            }
+        }
+
+        island.moveAll();
+
+        island.islandViewer();
+    }
+
+    public static AtomicInteger getTurnId() {
+        return id;
+    }
+}
